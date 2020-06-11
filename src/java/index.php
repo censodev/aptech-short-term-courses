58c06185ff5feb8cba6bda48d84249b018922c3a
JewCat <51000027+JewCat@users.noreply.github.com>
2020-06-11 22:06:46 +0700

    ggtag

--- src/java/index.php
+++ src/java/index.php
@@ -15,6 +15,16 @@
 <!Doctype html>
 <html lang="en">
   <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-20788510-60"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-20788510-60');
+    </script>
+
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
