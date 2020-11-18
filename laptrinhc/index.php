a850aeb5e1a2079b9ef1ffcc136747b7dffda6fa
Phuong Bui <51000027+censodev@users.noreply.github.com>
2020-11-18 10:27:48 +0700

    fix info

--- laptrinhc/index.php
+++ laptrinhc/index.php
@@ -659,7 +659,7 @@
     <footer class="text-white text-md-left text-center">
         <div class="container py-5 text-center">
             <h4 class="text-uppercase text-white text-center text-bold">Hệ thống đào tạo lập trình viên quốc tế Aptech</h4>
-            <div class="d-flex justify-content-center align-items-center pt-3">
+            <div class="d-flex justify-content-center align-items-center pt-3 flex-wrap">
                 <div class="px-3">
                     <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-geo-alt-fill text-warning" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
@@ -674,10 +674,10 @@
                 </div>
                 <div class="px-3"><span class="text-bold">212 - 214 Nguyễn Đình Chiểu</span>, Q.3</div>
             </div>
-            <a href="mailto:aptech3@aprotrain.com" class="btn btn-lg btn-warning mt-3 px-4" style="line-height:1;border-radius:28px;">
-                <span class="text-bold">Hotline: 1800 1147</span>
+            <a href="mailto:aptech1@aprotrain.com" class="btn btn-lg btn-warning mt-3 px-4" style="line-height:1;border-radius:28px;">
+                <span class="text-bold">Hotline: 1800 1141</span>
                 <br>
-                <small style="font-size:70%">Email: aptech3@aprotrain.com</small>
+                <small style="font-size:70%">Email: aptech1@aprotrain.com</small>
             </a>
         </div>
     </footer>
